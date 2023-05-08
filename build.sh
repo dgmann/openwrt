093f64467c5bc96d4c693f2051c063f396bd46c3
dgmann <dgmann@users.noreply.github.com>
2023-05-08 19:28:52 +0200

    add container support

--- build.sh
+++ build.sh
@@ -2,24 +2,21 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-PACKAGES="luci adguardhome htop irqbalance kmod-wireguard luci-app-bcp38 luci-app-sqm luci-app-wireguard luci-proto-wireguard sqm-scripts wireguard-tools luci-app-acme acme-dnsapi luci-ssl-nginx"
-
-# Copy configs
-mkdir -p "${SCRIPT_DIR}"/files/etc/config/
-scp -O root@192.168.0.4:/etc/config/network "${SCRIPT_DIR}"/files/etc/config/
-scp -O root@192.168.0.4:/etc/config/firewall "${SCRIPT_DIR}"/files/etc/config/
-
-chmod -R 755 "${SCRIPT_DIR}"/files/etc/config/
+PACKAGES="luci adguardhome htop irqbalance kmod-wireguard luci-app-bcp38 luci-app-sqm luci-app-wireguard luci-proto-wireguard sqm-scripts wireguard-tools luci-app-acme acme-dnsapi"
 
 mkdir -p "${SCRIPT_DIR}"/files/usr/bin/
 
-wget -O "${SCRIPT_DIR}"/files/usr/bin/netclient https://github.com/gravitl/netclient/releases/download/v0.18.5/netclient_linux_arm64 && \
+wget -O "${SCRIPT_DIR}"/files/usr/bin/netclient https://github.com/gravitl/netclient/releases/download/v0.18.7/netclient-linux-arm64 && \
     chmod +x "${SCRIPT_DIR}"/files/usr/bin/netclient
 
 wget -O "${SCRIPT_DIR}"/files/usr/bin/cloudflared https://github.com/cloudflare/cloudflared/releases/download/2023.1.0/cloudflared-linux-arm64 && \
     chmod +x "${SCRIPT_DIR}"/files/usr/bin/cloudflared
 
+mkdir "${SCRIPT_DIR}"/bin/
+IMAGE=openwrt/imagebuilder:armvirt-64-openwrt-22.03
+podman pull "$IMAGE"
 podman run --rm -it \
-    -v "${SCRIPT_DIR}"/bin/:/home/build/openwrt/bin:z \
-    -v "${SCRIPT_DIR}"/files/:/home/build/openwrt/files:z \
-    openwrtorg/imagebuilder:armvirt-64-22.03.3 make image PACKAGES="${PACKAGES}" FILES="files"
+    -v "${SCRIPT_DIR}"/bin/:/builder/bin:z \
+    -v "${SCRIPT_DIR}"/files/:/builder/files:z \
+    -v "${SCRIPT_DIR}"/.config:/builder/.config:z \
+    "$IMAGE" make image PACKAGES="${PACKAGES}" FILES="files"
