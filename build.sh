6ea109a1219ab9a9fb06a70b309d6cc0e7eeee9f
dgmann <dgmann@users.noreply.github.com>
2023-06-01 13:15:41 +0200

    feat: add config to config.yaml

--- build.sh
+++ build.sh
@@ -1,23 +1,6 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "Adding Packages: ${PACKAGES}"
 
-PACKAGES=$(yq '.packages | join(" ")' config.yaml)
-
-mkdir -p "${SCRIPT_DIR}"/files/usr/bin/
-
-wget -O "${SCRIPT_DIR}"/files/usr/bin/netclient https://github.com/gravitl/netclient/releases/download/v0.18.7/netclient-linux-arm64 && \
-    chmod +x "${SCRIPT_DIR}"/files/usr/bin/netclient
-
-wget -O "${SCRIPT_DIR}"/files/usr/bin/cloudflared https://github.com/cloudflare/cloudflared/releases/download/2023.1.0/cloudflared-linux-arm64 && \
-    chmod +x "${SCRIPT_DIR}"/files/usr/bin/cloudflared
-
-mkdir "${SCRIPT_DIR}"/bin/
-IMAGE=openwrt/imagebuilder:armvirt-64-openwrt-22.03
-podman pull "$IMAGE"
-sudo rm -rf "${SCRIPT_DIR}"/bin/*
-podman run --rm -it \
-    -v "${SCRIPT_DIR}"/bin/:/builder/bin:z \
-    -v "${SCRIPT_DIR}"/files/:/builder/files:z \
-    -v "${SCRIPT_DIR}"/scripts/build.sh:/builder/build.sh:z \
-    "$IMAGE" ./build.sh 
+sed -i "${CONFIG}" .config
+make image PACKAGES="${PACKAGES}" FILES="files"
--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Adding Packages: ${PACKAGES}"
-
-sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=.*/CONFIG_TARGET_ROOTFS_PARTSIZE=150/' .config
-make image PACKAGES="${PACKAGES}" FILES="files"
