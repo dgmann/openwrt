0453b70ca848d52d174f656bbbafec26d4584154
dgmann <dgmann@users.noreply.github.com>
2023-06-01 13:44:44 +0200

    feat: create release

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ echo "Adding Packages: ${PACKAGES}"
 
 sed -i "${CONFIG}" .config
 make image PACKAGES="${PACKAGES}" FILES="files"
+
+mv bin/targets/armvirt/64/*-default-rootfs.tar.gz bin/targets/armvirt/64/default-rootfs.tar.gz
+mv bin/targets/armvirt/64/*rootfs-squashfs.img.gz bin/targets/armvirt/64/rootfs-squashfs.img.gz 
+mv bin/targets/armvirt/64/*Image bin/targets/armvirt/64/Image
