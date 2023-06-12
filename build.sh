d2e054c1873e9010dc70af4329bd180d6e774225
dgmann <dgmann@users.noreply.github.com>
2023-06-12 14:53:23 +0200

    fix: pass through config env variable

--- build.sh
+++ build.sh
@@ -4,6 +4,4 @@ echo "Adding Packages: ${PACKAGES}"
 
 echo "Adapting .config"
 sed -i "${CONFIG}" .config
-echo "::debug::.config:"
-echo "::debug::$(cat .config)"
 make image PACKAGES="${PACKAGES}" FILES="files"
