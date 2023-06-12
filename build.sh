5617dc75cf898ce050b34572f3369f756afb25e5
dgmann <dgmann@users.noreply.github.com>
2023-06-12 14:49:45 +0200

    fix: add debug stmt

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 
 echo "Adding Packages: ${PACKAGES}"
 
+echo "Adapting .config"
 sed -i "${CONFIG}" .config
+echo "::debug::.config:"
+echo "::debug::$(cat .config)"
 make image PACKAGES="${PACKAGES}" FILES="files"
